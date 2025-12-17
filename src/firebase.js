e5ee4ef19ea3d5f28959389eea7e29c54f187139
marsellut56 <marsellut56@gmail.com>
2025-12-17 18:47:37 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,9 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID"
+  apiKey: "AIzaSyCXxIQD1aNxBvauiY65MMNWuwuXQYeKUn8",
+  authDomain: "mini-app-forcouples.firebaseapp.com",
+  databaseURL: "https://mini-app-forcouples-default-rtdb.firebaseio.com",
+  projectId: "mini-app-forcouples",
+  storageBucket: "mini-app-forcouples.firebasestorage.app",
+  messagingSenderId: "597378655994",
+  appId: "1:597378655994:web:82c08f4e0a81a6894ecb43",
+  measurementId: "G-6DM9GWM2QR"
 };
 
 const app = initializeApp(firebaseConfig);
